9e5e8968b1f5a5458fd07039b18977a0d0a7b7f5
saftaandika54 <saftaandika54@gmail.com>
2026-07-16 03:55:22 +0800

    Pertemuan 4: CRUD

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,7 +12,8 @@ class StudentController extends Controller
      */
     public function index()
 {
-    $students = Student::all();
+    $students = Student::latest()->get();
+    // students = Student::orderBy('name', 'asc')->get();
 
     return view('student.index', [
         'title' => 'Data Mahasiswa',
@@ -33,7 +34,19 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => ['required','max:255'],
+            'nim' => ['required','digits:11', 'numeric'],
+        ], [
+            'name.required' => 'Nama tidak boleh kosong',
+            'name.max' => 'Nama tidak boleh lebih dari :max karakter',
+            'nim.required' => 'NIM tidak boleh kosong',
+            'nim.digits' => 'NIM wajib :digits digit',
+            'nim.numeric' => 'NIM wajib angka',
+        ]);
+
+    Student::create($validated);
+    return to_route('student.index')->withSuccess('Data berhasil ditambahkan');
     }
 
     /**
@@ -49,7 +62,10 @@ class StudentController extends Controller
      */
     public function edit(Student $student)
     {
-        //
+            return view('student.edit', [
+        'title' => 'Edit Mahasiswa',
+        'student' => $student,
+        ]);
     }
 
     /**
@@ -57,7 +73,19 @@ class StudentController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+                $validated = $request->validate([
+            'name' => ['required','max:255'],
+            'nim' => ['required','digits:11', 'numeric'],
+        ], [
+            'name.required' => 'Nama tidak boleh kosong',
+            'name.max' => 'Nama tidak boleh lebih dari :max karakter',
+            'nim.required' => 'NIM tidak boleh kosong',
+            'nim.digits' => 'NIM wajib :digits digit',
+            'nim.numeric' => 'NIM wajib angka',
+        ]);
+
+    $student->update($validated);
+    return to_route('student.index')->withSuccess('Data berhasil diubah');
     }
 
     /**
@@ -65,6 +93,7 @@ class StudentController extends Controller
      */
     public function destroy(Student $student)
     {
-        //
+        $student->delete($student);
+        return to_route('student.index')->withSuccess('Data berhasil dihapus');
     }
 }
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -2,9 +2,26 @@
 
     <x-slot:title>{{ $title }}</x-slot>
 
+    @session('success')
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endsession
+
+    <a class="btn btn-primary mb-3" href="{{ route('student.create') }}" role="button">Create</a>
+
     <ul class="list-group">
         @foreach ($students as $student)
-            <li class="list-group-item">{{ $loop->iteration }}. {{ $student->nim }} -- {{ $student->name }}
+            <li class="list-group-item"><a class="btn btn-warning btn-sm" href="{{ route('student.edit', $student) }}"
+                    {{ $loop->iteration }}. {{ $student->nim }} -- {{ $student->name }} role="button">edit</a>
+                <form action="{{ route('student.destroy', $student) }}" method="POST" class="d-inline">
+                    @method('DELETE')
+                    @csrf
+
+                    <button type="submit" class="btn btn-danger btn-sm"
+                        onclick="return confirm('Anda yakin?')">Delete</button>
+                </form>
+
             </li>
         @endforeach
     </ul>
